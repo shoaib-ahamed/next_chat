2058a4af64fffa4ca01ebb7bd9a48afb44597fad
shoaibnesoi <shoaib@nesoi.one>
2023-02-04 14:03:22 +0600

    slight changes

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,19 @@
 import Head from "next/head";
+import { useRouter } from 'next/router';
+import { useEffect } from "react";
 import Layout from "../layout/layout";
 import styles from "../styles/Home.module.css";
 
+
 const Home = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if(!localStorage.getItem("CurrentUser")){
+      router.push('/login')
+    }
+  }, [router]);
+
   return (
     <div className={styles.container}>
       <Head>
